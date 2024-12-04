b261df43717886879fc3bae36e38e6beac60b216
hayder <hayderbd4290@gmail.com>
2024-12-04 23:33:07 +0600

    mongodb Atlas Added and secured

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+
+require('dotenv').config()
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASSWORD}@cluster0.7ya1e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function start() {
+  try {
+   
+    const campaignCollection = client.db("crowedDB").collection("campaignData");
+   
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  }
+}
+start().catch(console.dir);
+
 
 
 app.get('/', (req, res)=>{
